aea445c1a1eb22c09f16b7ac454157b3b84ba7f3
Sang Kim <slopsang1@gmail.com>
2015-11-02 14:17:38 -0800

    monday class

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,13 @@
 // Create application
-
+var myApp = angular.module('myApp', [])
 
 // Bind controller madCtrl
-
+myApp.controller('madCtrl', function($scope) {
+    $scope.name = 'name' 
+    $scope.age = 'age'
+    $scope.location = 'location'
+    $scope.food = 'food'
+})
 
 	// Set initial values for name, age, location, and food
 
